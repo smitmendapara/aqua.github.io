83466fe8d209e7b5291fed427932644c5f1c474c
Smit Mendapara <smitmendapara0910@gmail.com>
2021-12-27 21:44:26 +0530

    aqua saloon

---
+++ give.php
@@ -0,0 +1,9 @@
+<?php
+	$fname = $_POST['fname'];
+	$email = $_POST['email'];
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+	$item = $_POST['item'];
+	$number = $_POST['number'];
+	$message = $_POST['message'];
+?>
